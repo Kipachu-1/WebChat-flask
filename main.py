370bf2d22fd950a162accc8ae5da22046a65c0b1
Kipachu <103853361+Kipachu-1@users.noreply.github.com>
2022-08-11 21:03:52 +0600

    commit

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from flask import Flask, request, render_template, url_for
 from flask_socketio import SocketIO, send
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
+db = SQLAlchemy()
 app = Flask(__name__)
 app.config['SECRET KEY'] = 'secret123'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 socketio = SocketIO(app, cors_allowed_origins='*')
+login_manager = LoginManager()
+
+
+
+
 
 @socketio.on('message')
 def handle_message(message):
@@ -14,10 +23,14 @@ def handle_message(message):
         send(message=message, broadcast=True)
 
 
+@app.route('/login')
+def login():
+    return render_template('login_page.html')
+
 @app.route('/')
 def main_page():
     return render_template('main_page.html')
 
 if __name__ == '__main__':
-    socketio.run(app, host='localhost')
+    socketio.run(app, host='172.20.10.2')
     
