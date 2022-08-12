ada526590b86ddf5e826bbfe0a6ee2e276c9e687
Kipachu <103853361+Kipachu-1@users.noreply.github.com>
2022-08-12 21:44:31 +0600

    commit

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
 from flask_socketio import SocketIO, send
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from Base import *
 
 
-db = SQLAlchemy()
 app = Flask(__name__)
 app.config['SECRET KEY'] = 'secret123'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 socketio = SocketIO(app, cors_allowed_origins='*')
 login_manager = LoginManager()
-
+db.init_app(app)
 
 
 
@@ -23,9 +23,30 @@ def handle_message(message):
         send(message=message, broadcast=True)
 
 
-@app.route('/login')
+@app.route('/login', methods = ['POST', 'GET'])
 def login():
+    if(request.method == 'POST'):
+        Username = request.form.get('Username')
+        Password = request.form.get('Password')
+        try:
+            data = User.query.filter_by(Username=f'{Username}').all()[0]
+            if Username == data.Username and Password == data.Password:
+                return  redirect('/success')
+            else:
+                return redirect('/wrong')
+        except:
+            return redirect('/wrong')
+        
     return render_template('login_page.html')
+            
+            
+@app.route('/wrong')
+def wrong():
+    return 'wrong pass or username!'            
+            
+@app.route('/success')
+def success():
+    return 'it is working!'       
 
 @app.route('/')
 def main_page():
