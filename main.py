072e9ee76500a869008ced31a6179c5f6f9c6c9e
Kipachu <103853361+Kipachu-1@users.noreply.github.com>
2022-08-19 15:54:06 +0600

    he

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 
 
 app = Flask(__name__)
-app.secret_key = 'ajcinfuiehadiawd99481rr327982dj293'
+app.config['SEKRET_KEY'] = os.environ.get("SEKRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///test.sqlite3')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 socketio = SocketIO(app, cors_allowed_origins='*')
